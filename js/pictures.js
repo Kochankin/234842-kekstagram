0130831c081aca776a11e1e3f353dc5b64ad54dd
Kochanova Marianna <marian110791@mail.ru>
2017-12-20 00:40:26 +0300

    initialized pictures filters

--- js/pictures.js
+++ js/pictures.js
@@ -9,6 +9,15 @@
   var likesTemplate = template.content.querySelector('.picture-likes');
 
   var picturesContainer = document.querySelector('.pictures');
+  var filtersForm = document.body.querySelector('.filters');
+  var recommendButton = filtersForm.querySelector('#filter-recommend');
+  var popularButton = filtersForm.querySelector('#filter-popular');
+  var discussedButton = filtersForm.querySelector('#filter-discussed');
+  var randomButton = filtersForm.querySelector('#filter-random');
+  var loadedArray;
+  var originPicturesArray;
+  var DEBOUNCE_INTERVAL = 500;
+  var lastTimeout;
 
   // функция для вставки данных из массива в шаблон
   function getPicture(photoData) {
@@ -24,11 +33,15 @@
     for (var i = 0; i < pictureData.length; i++) {
       documentFragment.appendChild(getPicture(pictureData[i]));
     }
-    window.pictures.picturesContainer.appendChild(documentFragment);
+    picturesContainer.appendChild(documentFragment);
   }
 
+  // при успешной загрузке изображений (GET)
   function onLoadGet(response) {
     renderPictures(response);
+    filtersForm.classList.remove('filters-inactive');
+    loadedArray = response;
+    originPicturesArray = loadedArray.slice();
   }
 
   // ошибка при запросе картинок (GET)
@@ -36,10 +49,84 @@
     window.form.renderErrorDiv(document.body, response);
   }
 
+  // для устранения дребезга
+  function debounce(func) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
+  }
+
+  // функция по созданию нового массива со случайным порядком элементов внутри
+  function makeRandomArray(array) {
+    var newArray = [];
+    array.forEach(function (currentValue) {
+      var newIndex = window.utils.getRandomInt(0, (array.length - 1));
+      while (newArray[newIndex]) {
+        newIndex = window.utils.getRandomInt(0, (array.length - 1));
+      }
+      newArray[newIndex] = currentValue;
+    });
+    return newArray;
+  }
+
+  // обработчик для кнопки РЕКОМЕНДУЕМЫЕ
+  function onRecommendButtonClick() {
+    picturesContainer.innerHTML = '';
+    renderPictures(originPicturesArray);
+  }
+  // обработчик для кнопки ПОПУЛЯРНЫЕ
+  function onPopularButtonClick() {
+    picturesContainer.innerHTML = '';
+    var newPicturesArray = loadedArray.sort(function (a, b) {
+      var sorted = b.likes - a.likes;
+      if (sorted === 0) {
+        sorted = b.comments.length - a.comments.length;
+      }
+      return sorted;
+    });
+    renderPictures(newPicturesArray);
+  }
+  // обработчик для кнопки ОБСУЖДАЕМЫЕ
+  function onDiscussedButtonClick() {
+    picturesContainer.innerHTML = '';
+    var newPicturesArray = loadedArray.sort(function (a, b) {
+      var sorted = b.comments.length - a.comments.length;
+      if (sorted === 0) {
+        sorted = b.likes - a.likes;
+      }
+      return sorted;
+    });
+    renderPictures(newPicturesArray);
+  }
+  // обработчик для кнопки СЛУЧАЙНЫЕ
+  function onRandomButtonClick() {
+    var newPicturesArray = makeRandomArray(loadedArray);
+    picturesContainer.innerHTML = '';
+    renderPictures(newPicturesArray);
+  }
+
+  // инициация обработчиков
+  function filtersButtonInit() {
+    randomButton.addEventListener('click', function () {
+      debounce(onRandomButtonClick);
+    });
+    popularButton.addEventListener('click', function () {
+      debounce(onPopularButtonClick);
+    });
+    discussedButton.addEventListener('click', function () {
+      debounce(onDiscussedButtonClick);
+    });
+    recommendButton.addEventListener('click', function () {
+      debounce(onRecommendButtonClick);
+    });
+  }
+
+  filtersButtonInit();
+
   window.pictures = {
     onLoadGet: onLoadGet,
     onErrorGet: onErrorGet,
-    // переменная контейнера, куда будут вставлены миниатюры
     picturesContainer: picturesContainer,
     renderPictures: renderPictures
   };
